001fab8ec2950bbfb677149dea7c364700210d69
PowerLands <powerlandss@vk.com>
2022-09-18 14:40:06 +0300

    Start of development

--- src/main/java/ru/powerlands/back/LandsBackup.java
+++ src/main/java/ru/powerlands/back/LandsBackup.java
@@ -6,15 +6,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.IOException;
 
 public final class LandsBackup extends JavaPlugin {
+    private static LandsBackup Main;
 
     @Override
     public void onEnable() {
-
+        saveDefaultConfig();
+        setMain(this);
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static LandsBackup getMain() {
+        return Main;
+    }
+
+    private static void setMain(LandsBackup main) {
+        Main = main;
     }
 }
