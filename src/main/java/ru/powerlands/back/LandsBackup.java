113bff183cd9a0b4c4d669477723771fb49a2aee
PowerLands <powerlandss@vk.com>
2022-09-18 14:31:01 +0300

    Initial commit

---
+++ src/main/java/ru/powerlands/back/LandsBackup.java
@@ -0,0 +1,20 @@
+package ru.powerlands.back;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+
+public final class LandsBackup extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
